630f86793741f47216004accbf9eee63ecb038d3
Tracy Reuther <tracyreuther@gmail.com>
2019-08-21 01:03:52 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = title: "Home Alone" release_date: 1990)
   # release_date == 1990
 
   Movie.create do |m|
-    m.title == "Home Alone"
+    m.title == args[title:]
     m.release_date == 1990
   end
 end
