bd9574302de6bb1dca4d1b0b255fba18840c12b7
Tracy Reuther <tracyreuther@gmail.com>
2019-08-21 00:25:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = "The Room", 2003)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
